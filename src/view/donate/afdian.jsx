a3fe18c67a498f9d141e4c8e9fc4db82818400c6
ppoffice <ppoffice@users.noreply.github.com>
2020-08-17 21:10:59 -0400

    feat(donate): add afdian.net button

---
+++ src/view/donate/afdian.jsx
@@ -0,0 +1,68 @@
+/**
+ * Afdian.net donation JSX component.
+ * @module view/donate/afdian
+ */
+const { Component } = require('inferno');
+const { cacheComponent } = require('../../util/cache');
+
+/**
+ * Afdian donation JSX component.
+ *
+ * @example
+ * <Afdian
+ *     title="******"
+ *     url="/path/to/url" />
+ */
+class Afdian extends Component {
+  render() {
+    const { title, url } = this.props;
+    if (!url) {
+      return (
+        <div class="notification is-danger">
+          You forgot to set the <code>url</code> for Afdian. Please set it in{' '}
+          <code>_config.yml</code>.
+        </div>
+      );
+    }
+    return (
+      <a
+        class="button donate"
+        href={url}
+        style={{
+          'background-color': '#885fd9',
+          'border-color': 'transparent',
+          color: 'white',
+        }}
+        target="_blank"
+        rel="noopener">
+        <span class="icon is-small">
+          <i class="fas fa-charging-station"></i>
+        </span>
+        <span>{title}</span>
+      </a>
+    );
+  }
+}
+
+/**
+ * Cacheable Afdian donation JSX component.
+ * <p>
+ * This class is supposed to be used in combination with the <code>locals</code> hexo filter
+ * ({@link module:hexo/filter/locals}).
+ *
+ * @see module:util/cache.cacheComponent
+ * @example
+ * <Afdian.Cacheable
+ *     donate={{ type: 'afdian', url='******' }}
+ *     helper={{ __: function() {...} }} />
+ */
+Afdian.Cacheable = cacheComponent(Afdian, 'donate.afdian', (props) => {
+  const { donate, helper } = props;
+
+  return {
+    title: helper.__('donate.' + donate.type),
+    url: helper.url_for(donate.url),
+  };
+});
+
+module.exports = Afdian;
