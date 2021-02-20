b551cc3d5a213c388ff9750607bf0f73c00389ff
marksylee <me@marksylee.com>
2021-02-20 11:09:24 +0800

    update test

--- src/hexo/helper/cdn.test.js
+++ src/hexo/helper/cdn.test.js
@@ -204,7 +204,7 @@ describe('Get icon font URL', () => {
 
   test('fontawesome', () => {
     hexo.config.providers = { iconcdn: 'fontawesome' };
-    const expected = ['https://use.fontawesome.com/releases/v5.12.0/css/all.css'];
+    const expected = ['https://use.fontawesome.com/releases/v5.15.2/css/all.css'];
     cases.forEach((func, i) => expect(func()).toBe(expected[i]));
   });
 
