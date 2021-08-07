74bb8a50b30d8441ee6b6484d79c69d04a4fd61e
绿色毛怪 <37977109+GreenSulley@users.noreply.github.com>
2021-08-07 17:31:08 +0800

    feat(view): comment disqus lazyload
    https://blog.skk.moe/post/prevent-disqus-from-slowing-your-site/

--- src/view/comment/disqus.jsx
+++ src/view/comment/disqus.jsx
@@ -17,38 +17,56 @@ const { cacheComponent } = require('../../util/cache');
  *     path="/path/to/page" />
  */
 class Disqus extends Component {
-  render() {
-    const { shortname, disqusId, path, permalink } = this.props;
-    if (!shortname) {
-      return (
-        <div class="notification is-danger">
-          You forgot to set the <code>shortname</code> for Disqus. Please set it in{' '}
-          <code>_config.yml</code>.
-        </div>
-      );
+    render() {
+        const { shortname, disqusId, path, permalink } = this.props;
+        if (!shortname) {
+            return (
+                <div class="notification is-danger">
+                    You forgot to set the <code>shortname</code> for Disqus. Please set it in{' '}
+                    <code>_config.yml</code>.
+                </div>
+            );
+        }
+        const js = `function loadDisqus() {
+            var disqus_config = function () {
+                this.page.url = '${permalink}';
+                this.page.identifier = '${disqusId || path}';
+            };
+            (function() {
+                var d = document, s = d.createElement('script');  
+                s.src = '//' + '${shortname}' + '.disqus.com/embed.js';
+                s.setAttribute('data-timestamp', +new Date());
+                (d.head || d.body).appendChild(s);
+            })();
+        }
+        var runningOnBrowser = typeof window !== "undefined";
+        var isBot = runningOnBrowser && !("onscroll" in window) || typeof navigator !== "undefined" && /(gle|ing|ro|msn)bot|crawl|spider|yand|duckgo/i.test(navigator.userAgent);
+        var supportsIntersectionObserver = runningOnBrowser && "IntersectionObserver" in window;
+        setTimeout(function () {
+          if (!isBot && supportsIntersectionObserver) {
+            var disqus_observer = new IntersectionObserver(function(entries) {
+              if (entries[0].isIntersecting) {
+                loadDisqus();
+                disqus_observer.disconnect();
+              }
+            }, { threshold: [0] });
+            disqus_observer.observe(document.getElementById('disqus_thread'));
+          } else {
+            loadDisqus();
+          }
+        }, 1);`;
+        return (
+            <>
+                <div id="disqus_thread">
+                    <noscript>
+                        Please enable JavaScript to view the{' '}
+                        <a href="//disqus.com/?ref_noscript">comments powered by Disqus.</a>
+                    </noscript>
+                </div>
+                <script dangerouslySetInnerHTML={{ __html: js }}></script>
+            </>
+        );
     }
-    const js = `var disqus_config = function () {
-            this.page.url = '${permalink}';
-            this.page.identifier = '${disqusId || path}';
-        };
-        (function() {
-            var d = document, s = d.createElement('script');  
-            s.src = '//' + '${shortname}' + '.disqus.com/embed.js';
-            s.setAttribute('data-timestamp', +new Date());
-            (d.head || d.body).appendChild(s);
-        })();`;
-    return (
-      <>
-        <div id="disqus_thread">
-          <noscript>
-            Please enable JavaScript to view the{' '}
-            <a href="//disqus.com/?ref_noscript">comments powered by Disqus.</a>
-          </noscript>
-        </div>
-        <script dangerouslySetInnerHTML={{ __html: js }}></script>
-      </>
-    );
-  }
 }
 
 /**
@@ -68,14 +86,14 @@ class Disqus extends Component {
  *     }} />
  */
 Disqus.Cacheable = cacheComponent(Disqus, 'comment.disqus', (props) => {
-  const { comment, page } = props;
+    const { comment, page } = props;
 
-  return {
-    path: page.path,
-    shortname: comment.shortname,
-    disqusId: page.disqusId,
-    permalink: page.permalink,
-  };
+    return {
+        path: page.path,
+        shortname: comment.shortname,
+        disqusId: page.disqusId,
+        permalink: page.permalink,
+    };
 });
 
 module.exports = Disqus;
