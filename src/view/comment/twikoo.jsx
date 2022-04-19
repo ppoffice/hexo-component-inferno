54f01ca86ec3bc601049f2c8a5bd4056b7755864
Nofated <49985975+Nofated095@users.noreply.github.com>
2022-04-19 17:03:12 +0800

    Update twikoo.jsx

--- src/view/comment/twikoo.jsx
+++ src/view/comment/twikoo.jsx
@@ -1,6 +1,19 @@
+/**
+ * Twikoo comment JSX component.
+ * @module view/comment/twikoo
+ */
 const { Component, Fragment } = require('inferno');
 const { cacheComponent } = require('hexo-component-inferno/lib/util/cache');
 
+/**
+ * Twikoo comment JSX component.
+ *
+ * @see https://twikoo.js.org/quick-start.html
+ * @example
+ * <Twikoo
+ *     env_id="Vercel Domain"
+ *     jsUrl="/path/to/Twikoo.js" />
+ */
 class Twikoo extends Component {
   render() {
     const {
@@ -20,6 +33,20 @@ class Twikoo extends Component {
   }
 }
 
+/**
+ * Cacheable Twikoo comment JSX component.
+ * <p>
+ * This class is supposed to be used in combination with the <code>locals</code> hexo filter
+ * ({@link module:hexo/filter/locals}).
+ *
+ * @see module:util/cache.cacheComponent
+ * @example
+ * <Twikoo.Cacheable
+ *     comment={{
+ *         env_id: "https://path/to/vercel/domain"
+ *         jsURL: "https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-M/twikoo/1.4.18/twikoo.all.min.js"
+ *     }}
+ */
 Twikoo.Cacheable = cacheComponent(Twikoo, 'comment.twikoo', (props) => {
   const { comment } = props;
   return {
